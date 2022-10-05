09349193b5e7a42948d05e889d297996cd693535
MournfulFrog <mournfulfrog@noreply@snopyta.org>
2022-10-05 18:21:07 +0100

    refactor: bash aliases

--- .local/scripts/backup.sh
+++ .local/scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -av --delete --exclude={/home/jb/.cache,/home/jb/.local/share/containers,/home/jb/.var/app/com.valvesoftware.Steam,/home/jb/Games} /home /run/media/jb/BackupPlus/surf@arch
+rsync -av --delete --exclude={/home/jb/.cache,/home/jb/.local/share/containers,/home/jb/.local/share/libvirt/images,/home/jb/.local/src,/home/jb/.var/app/com.valvesoftware.Steam,/home/jb/Games} /home /run/media/jb/BackupPlus/surf@arch
