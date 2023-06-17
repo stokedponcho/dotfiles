4354e576e259324b71f87574d4575fd3d991a24b
MournfulFrog <mournfulfrog@noreply@snopyta.org>
2023-06-17 08:54:32 +0100

    feat: remove system install, focus on .dotfiles

--- conf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp etc/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
--- dotfiles.sh
+++ dotfiles.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-stow . -v
-[[ -d "/usr/share/libalpm/hooks" ]] && stow pacman-hooks --target="/usr/share/libalpm/hooks" -v
+stow . -v  \
+	--ignore .stowrc					\
+	--ignore .disabled				\
+	--ignore .gitmodules			\
+	--ignore dotfiles.sh			\
+	--ignore README.md
